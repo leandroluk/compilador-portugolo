25cac1b81f0972721d9d88ea5c6609ffd937e7f9
Leandro <leandroluk@gmail.com>
2018-11-27 10:27:45 -0200

    1

--- src/Lexem.ts
+++ src/Lexem.ts
@@ -1,4 +1,6 @@
-import { Ts } from './Ts'
+import { Ts } from './Ts';
+import { Token } from './Token';
+
 /**
  * classe que faz a validação léxica
  * @param {string} filepath
@@ -9,16 +11,127 @@ export class Lexem {
     private _lookahead: number = 0;
     private _row: number = 1;
     private _col: number = 1;
-    private _file: string = null;
-    private _ts: Ts = new Ts();
+    private _words: string[] = null;
+    private _ts: Ts;
     private _errors: Error[] = [];
 
     constructor(file: string) {
         try {
-
-        } catch (error) {
-
+            this._words = file.split('');
+            this._ts = new Ts();
+        } catch (e) {
+            throw e;
         }
     }
 
+    /**
+     * cria os erros e armazena em um banco de dados
+     * @param word 
+     * @param expect 
+     * @param row 
+     * @param col 
+     */
+    private catchError(word: string, expect: string, row: number, col: number) {
+
+        // cria o erro
+        let error = new Error(
+            `[Erro léxico ${this._errors.length + 1}: ` +
+            `Símbolo encontrado: ${word} na linha ${row} e coluna ${col}. ` +
+            `Símbolo esperado: ${expect}`
+        );
+
+        // armazena na lista de erros
+        this._errors.push(error)
+
+        // imprime a mensagem do erro sem parar o código
+        console.error(error.message);
+
+        // corrije as linhas caso o erro seja enviado em uma quebra de linha
+        this.checkLinebreak(word);
+
+    }
+
+    /**
+     * verifica se o caracter passado é uma quebra de linha e, 
+     * caso seja, corrije a linha e a coluna
+     * @param word 
+     */
+    private checkLinebreak(word: string): boolean {
+
+        if (['\n', '\r'].indexOf(word) > -1) {
+
+            this._row++;
+            this._col = 1;
+
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+    public nextToken(): Token {
+
+        let word: string = '';
+        let state: number = 0;
+        let c: string = null;
+
+        while (true) {
+
+            c = null;
+
+            try {
+
+                this._lookahead = !!this._words[this._lookahead] ? this._lookahead++ : -1;
+
+                if (this._lookahead !== this._eof) {
+                    c = this._words[this._lookahead];
+                    this._col++;
+                }
+
+            } catch (e) {
+                throw e;
+            }
+
+            switch (state) {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                case 12:
+                case 13:
+                case 14:
+                case 15:
+                case 16:
+                case 17:
+                case 18:
+                case 19:
+                case 20:
+                case 21:
+                case 22:
+                case 23:
+                case 24:
+                case 25:
+                case 26:
+                case 27:
+                case 28:
+                case 29:
+                case 30:
+                case 31:
+                case 32:
+                case 33:
+            }
+
+        }
+
+    }
 }
--- src/Ts.ts
+++ src/Ts.ts
@@ -3,33 +3,70 @@ import { Tag } from "./Tag";
 
 export class Ts {
 
-    private ts: { [key: string]: Token } = {}
+    private _ts: { [key: string]: Token } = {}
 
-    constructor(){
-        this.ts[Tag.KW_ALGORITMO] = new Token(Tag.KW_ALGORITMO, 'ALGORITMO');
-        this.ts[Tag.KW_DECLARE] = new Token(Tag.KW_DECLARE, 'DECLARE');
-        this.ts[Tag.KW_INICIO] = new Token(Tag.KW_INICIO, 'INICIO');
-        this.ts[Tag.KW_FIM] = new Token(Tag.KW_FIM, 'FIM');
-        this.ts[Tag.KW_SUBROTINA] = new Token(Tag.KW_SUBROTINA, 'SUBROTINA');
-        this.ts[Tag.KW_RETORNE] = new Token(Tag.KW_RETORNE, 'RETORNE');
-        this.ts[Tag.LOGICO] = new Token(Tag.LOGICO, 'LOGICO');
-        this.ts[Tag.NUMERICO] = new Token(Tag.NUMERICO, 'NUMERICO');
-        this.ts[Tag.LITERAL] = new Token(Tag.LITERAL, 'LITERAL');
-        this.ts[Tag.NULO] = new Token(Tag.NULO, 'NULO');
-        this.ts[Tag.KW_SE] = new Token(Tag.KW_SE, 'SE');
-        this.ts[Tag.KW_SENAO] = new Token(Tag.KW_SENAO, 'SENAO');
-        this.ts[Tag.KW_ENQUANTO] = new Token(Tag.KW_ENQUANTO, 'ENQUANTO');
-        this.ts[Tag.KW_PARA] = new Token(Tag.KW_PARA, 'PARA');
-        this.ts[Tag.KW_ATE] = new Token(Tag.KW_ATE, 'ATE');
-        this.ts[Tag.KW_FACA] = new Token(Tag.KW_FACA, 'FACA');
-        this.ts[Tag.KW_REPITA] = new Token(Tag.KW_REPITA, 'REPITA');
-        this.ts[Tag.KW_ESCREVA] = new Token(Tag.KW_ESCREVA, 'ESCREVA');
-        this.ts[Tag.KW_LEIA] = new Token(Tag.KW_LEIA, 'LEIA');
-        this.ts[Tag.KW_VERDADEIRO] = new Token(Tag.KW_VERDADEIRO, 'VERDADEIRO');
-        this.ts[Tag.KW_FALSO] = new Token(Tag.KW_FALSO, 'FALSO');
-        this.ts[Tag.KW_OU] = new Token(Tag.KW_OU, 'OU');
-        this.ts[Tag.KW_E] = new Token(Tag.KW_E, 'E');
-        this.ts[Tag.KW_NAO] = new Token(Tag.KW_NAO, 'NAO');
+    constructor() {
+        this._ts[Tag.KW_ALGORITMO] = new Token(Tag.KW_ALGORITMO, 'ALGORITMO');
+        this._ts[Tag.KW_DECLARE] = new Token(Tag.KW_DECLARE, 'DECLARE');
+        this._ts[Tag.KW_INICIO] = new Token(Tag.KW_INICIO, 'INICIO');
+        this._ts[Tag.KW_FIM] = new Token(Tag.KW_FIM, 'FIM');
+        this._ts[Tag.KW_SUBROTINA] = new Token(Tag.KW_SUBROTINA, 'SUBROTINA');
+        this._ts[Tag.KW_RETORNE] = new Token(Tag.KW_RETORNE, 'RETORNE');
+        this._ts[Tag.LOGICO] = new Token(Tag.LOGICO, 'LOGICO');
+        this._ts[Tag.NUMERICO] = new Token(Tag.NUMERICO, 'NUMERICO');
+        this._ts[Tag.LITERAL] = new Token(Tag.LITERAL, 'LITERAL');
+        this._ts[Tag.NULO] = new Token(Tag.NULO, 'NULO');
+        this._ts[Tag.KW_SE] = new Token(Tag.KW_SE, 'SE');
+        this._ts[Tag.KW_SENAO] = new Token(Tag.KW_SENAO, 'SENAO');
+        this._ts[Tag.KW_ENQUANTO] = new Token(Tag.KW_ENQUANTO, 'ENQUANTO');
+        this._ts[Tag.KW_PARA] = new Token(Tag.KW_PARA, 'PARA');
+        this._ts[Tag.KW_ATE] = new Token(Tag.KW_ATE, 'ATE');
+        this._ts[Tag.KW_FACA] = new Token(Tag.KW_FACA, 'FACA');
+        this._ts[Tag.KW_REPITA] = new Token(Tag.KW_REPITA, 'REPITA');
+        this._ts[Tag.KW_ESCREVA] = new Token(Tag.KW_ESCREVA, 'ESCREVA');
+        this._ts[Tag.KW_LEIA] = new Token(Tag.KW_LEIA, 'LEIA');
+        this._ts[Tag.KW_VERDADEIRO] = new Token(Tag.KW_VERDADEIRO, 'VERDADEIRO');
+        this._ts[Tag.KW_FALSO] = new Token(Tag.KW_FALSO, 'FALSO');
+        this._ts[Tag.KW_OU] = new Token(Tag.KW_OU, 'OU');
+        this._ts[Tag.KW_E] = new Token(Tag.KW_E, 'E');
+        this._ts[Tag.KW_NAO] = new Token(Tag.KW_NAO, 'NAO');
+    }
+
+    public put(tag: Tag, token: Token): void {
+        this._ts[tag] = token;
+    }
+
+    public get(lexem: string): Token {
+
+        let token: Token = null;
+
+        for (let i in this._ts) {
+            if (this._ts[i].lexem.toLowerCase() === lexem.toLowerCase()) {
+                token = this._ts[i];
+            }
+        }
+
+        return token;
+
+    }
+
+    public toString() {
+
+        const tsKeys = Object.keys(this._ts);
+
+        let result: string = '';
+
+        for (let i: 0; i < tsKeys.length; i++) {
+            result += `posição ${i + 1}:\t ${this._ts[tsKeys[i]].toString()}\n`;
+        }
+
+        return result;
+
+    }
+
+    public toConsole() {
+        console.log("\nTabelas de Símbolos da linguagem Portugolo:\n===========================================\n");
+        console.log(`${this.toString()}\n`)
     }
 
 }
