8d42c414ce7f30d20255e02a7280e6bbc8cb4c84
Leandro <leandroluk@gmail.com>
2018-11-30 14:46:15 -0200

    1

--- tests/main.ts
+++ tests/main.ts
@@ -8,11 +8,13 @@ import { Ts } from '../src/ts';
 
 let file = fs.readFileSync(path.join(__dirname, 'arquivo_de_teste.ptgl')).toString();
 
+// cria o lexema usando o arquivo de teste
 const lexem = new Lexem(file);
-//const parser = new Parser(lexem);
-//parser.Compilador();
 
+// faz o parse da análise léxica
+const parser = new Parser(lexem);
+parser.Compilador();
+
+// imprime a tabela de símbolos
 const ts = new Ts();
-
 ts.toConsole();
-
